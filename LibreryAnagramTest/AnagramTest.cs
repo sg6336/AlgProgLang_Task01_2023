687d6214dfaaf1b0735e7f817e4b9cc1a810eae9
Iryna Korniyenko <7917836@stud.nau.edu.ua>
2023-03-01 10:38:08 +0200

    Add new files

---
+++ LibreryAnagramTest/AnagramTest.cs
@@ -0,0 +1,16 @@
+namespace LibreryAnagramTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
